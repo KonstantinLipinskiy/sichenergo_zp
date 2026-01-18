4ab4bd63f0e553137e6dda3dd3b77bb94e69777c
KonstantinLipinskiy <nobles2007@ukr.net>
2026-01-18 19:56:50 +0200

    Added a news filter to the about.html page

--- company/views.py
+++ company/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import DocumentsInform, DocumentsItem, Presentation, ReviewsInfo, ReviewsItem
 from django.views import View
+from news.models import NewsItem
 
 class CompanyView(View):
 	def get(self, request):
@@ -8,10 +9,10 @@ class CompanyView(View):
 
 class AboutView(View):
 	def get(self, request):
-		present = Presentation.objects.all()
+		news = NewsItem.objects.filter(heading__in=['Компанія', 'Поза роботою', 'Виробництво']).order_by('-created_at').first()
 		context = {
 			"title": "Про нас",
-			"present": present,
+			"news": news,
 		}
 
 		return render(request, 'company/about.html', context)
