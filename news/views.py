d2c13f042eace005dd4fb4a5c01f68f5364d333b
KonstantinLipinskiy <nobles2007@ukr.net>
2026-01-18 16:08:57 +0200

    Added a paginator for the news page

--- news/views.py
+++ news/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.views import View
 from .models import NewsItem
+from django.core.paginator import Paginator
 
 class NewsView(View):
 	def get(self, request):
 		news_items = NewsItem.objects.all()
+		paginator = Paginator(news_items, 2)
+		page_number = request.GET.get("page")
+		page_obj = paginator.get_page(page_number)
 		context = {
 			"title": "Новини",
 			"news_items": news_items,
+			"page_obj": page_obj,
 		}
 
 		return render(request, "news/news.html", context)
