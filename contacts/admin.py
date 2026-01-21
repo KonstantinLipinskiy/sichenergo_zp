28e01716cfce784a3e54b999af52fb496cf3c62d
KonstantinLipinskiy <nobles2007@ukr.net>
2026-01-21 16:47:54 +0200

    Finished with the contacts page

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Contacts, ContactsLocation
 
-# Register your models here.
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+	list_display = ['telephone', 'e_mail', 'image', 'details', 'name']
+
+@admin.register(ContactsLocation)
+class ContactsLocationAdmin(admin.ModelAdmin):
+	list_display = ['title', 'name', 'map_url']
