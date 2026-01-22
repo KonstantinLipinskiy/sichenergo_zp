5b5ff7d1f316b14be0eac33c31c00bf50911eadb
KonstantinLipinskiy <nobles2007@ukr.net>
2026-01-22 18:29:46 +0200

    Wrote tests for contacts

---
+++ contacts/tests/test_views.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from django.urls import reverse
+from contacts.models import Contacts, ContactsLocation
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+class ContactsViewTest(TestCase):
+	def setUp(self):
+		Contacts.objects.create(
+			name='test',
+			telephone='555-44-22-33',
+			details='Реквізити',
+			e_mail='test@mail.com',
+			image=SimpleUploadedFile("test.jpg", b"file_content", content_type="image/jpeg")
+		)
+		ContactsLocation.objects.create(
+			title='Локація',
+			name='Фізичне місцезнаходження виробництва',
+			map_url='https://maps.google.com/?q=Kyiv'
+		)
+
+	def test_contacts_status_code(self):
+		url = reverse('contacts:contacts')
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+
+	def test_contacts_templates(self):
+		url = reverse('contacts:contacts')
+		response = self.client.get(url)
+		self.assertTemplateUsed(response, 'contacts/contacts.html')
+
+	def test_contacts_context(self):
+		url = reverse('contacts:contacts')
+		response = self.client.get(url)
+		self.assertIn("contact", response.context)
+		self.assertIn("location", response.context)
+		self.assertEqual(response.context["title"], "Контакти")
